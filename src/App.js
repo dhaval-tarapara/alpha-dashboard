9fe83e6b365ad8f7a906731b6faeda79bea58390
dhaval-tarapara <dhaval.tarapara6773@gmail.com>
2023-01-08 02:06:38 +0530

    Add: Theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
     <div className="app">
-      <h1>Hello world!</h1>
-      <Topbar />
-      <Sidebar />
-      <Dashboard />
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <Topbar/>
+              <Dashboard />
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </div>
   );
 }
