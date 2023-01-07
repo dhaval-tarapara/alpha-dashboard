7dfc407511f15efb9397a3a7e677205ae27ebc8d
dhaval-tarapara <dhaval.tarapara6773@gmail.com>
2023-01-08 01:10:49 +0530

    Implemented folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+
 function App() {
   return (
     <div className="app">
       <h1>Hello world!</h1>
+      <Topbar />
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/pages/global/Topbar.jsx
@@ -0,0 +1,9 @@
+const Topbar = () => {
+  return (
+    <div>
+      <div>Topbar</div>
+    </div>
+  );
+};
+
+export default Topbar;
